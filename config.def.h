092d3a06f68f9322369f634c1b691bde6a4ab293
matthewben-net <github@matthewben.net>
2024-10-13 20:38:43 -0400

    feat(Colorscheme): Catppuccin
    Added a few extra color-scheme labels so that I can better change colors of specific keys. Changed the arrow keys to be their own color, made the super key purple, made the enter key red,  made the alternative layout keys the same color as the background, and adjusted the fg and bg colors. Catppuccin Mocha was used for everything aside from the enter key as the latte color version looked better.

--- config.def.h
+++ config.def.h
@@ -7,23 +7,67 @@ static const int transparency = 255;
 
 struct clr_scheme schemes[] = {
 {
-  /* colors */
-  .bg = {.bgra = {15, 15, 15, transparency}},
-  .fg = {.bgra = {45, 45, 45, transparency}},
-  .high = {.bgra = {100, 100, 100, transparency}},
-  .swipe = {.bgra = {100, 255, 100, 64}},
-  .text = {.color = UINT32_MAX},
+  /* colors for main keys */
+  .bg = {.bgra = {27, 17, 17, transparency}}, //Mocha Crust
+  .fg = {.bgra = {37, 24, 24, transparency}}, //Mocha Mantle
+  .high = {.bgra = {46, 30, 30, transparency}}, //Mocha Base
+  .swipe = {.bgra = {46, 30, 30, 64}}, //Mocha Base
+  .text = {.bgra = {244, 214, 205, transparency}}, //Mocha Text
   .font = DEFAULT_FONT,
   .rounding = DEFAULT_ROUNDING,
 },
 {
-  /* colors */
-  .bg = {.bgra = {15, 15, 15, transparency}},
-  .fg = {.bgra = {32, 32, 32, transparency}},
-  .high = {.bgra = {100, 100, 100, transparency}},
-  .swipe = {.bgra = {100, 255, 100, 64}},
-  .text = {.color = UINT32_MAX},
+  /* colors for external perimeter keys*/
+  .fg = {.bgra = {68, 50, 49, transparency}}, //Mocha Surface0
+  .high = {.bgra = {90, 71, 69, transparency}}, //Mocha Surface1
+  .swipe = {.bgra = {90, 71, 69, 64}}, //Mocha Surface1
+  .text = {.bgra = {244, 214, 205, transparency}}, //Mocha Text
   .font = DEFAULT_FONT,
+  .rounding = DEFAULT_ROUNDING,
+},
+{
+  /* color for enter button */
+  .fg = {.bgra = {132, 130, 231, transparency}}, //Frappe Red
+  .high = {.bgra = {156, 153, 234, transparency}},//Frappe Maroon
+  .swipe = {.bgra = {156, 153, 234, 64}},//Frappe Maroon
+  .text = {.bgra = {27, 17, 17, transparency}}, //Mocha Crust
+  .font = DEFAULT_FONT,
+  .rounding = DEFAULT_ROUNDING,
+},
+{
+  /* color for arrow keys */
+  .fg = {.bgra = {250, 180, 137, transparency}}, //Mocha Blue
+  .high = {.bgra = {236, 199, 116, transparency}}, //Mocha Sapphire
+  .swipe = {.bgra = {236, 199, 116, 64}}, //Mocha Sapphire
+  .text = {.bgra = {27, 17, 17, transparency}}, //Mocha Crust
+  .font = DEFAULT_FONT,
+  .rounding = DEFAULT_ROUNDING,
+},
+{
+  /* color for additional layouts buttons */
+  .fg = {.bgra = {27, 17, 17, transparency}}, //Mocha Crust
+  .high = {.bgra = {46, 30, 30, transparency}}, //Mocha Base
+  .swipe = {.bgra = {46, 30, 30, 64}}, //Mocha Base
+  .text = {.bgra = {244, 214, 205, transparency}}, //Mocha Text
+  .font = DEFAULT_FONT,
+  .rounding = DEFAULT_ROUNDING,
+},
+{
+  /* color for super button */
+  .fg = {.bgra = {239, 57, 136, transparency}}, //Frappe Muave
+  .high = {.bgra = {203, 118, 234, transparency}}, //Frappe Pink
+  .swipe = {.bgra = {203, 118, 234, 64}}, //Frappe Pink
+  .text = {.bgra = {27, 17, 17, transparency}}, //Mocha Crust
+  .font = DEFAULT_FONT,
+  .rounding = DEFAULT_ROUNDING,
+},
+{
+  /* color for escape button (this is just to increase the font size) */
+  .fg = {.bgra = {68, 50, 49, transparency}}, //Mocha Surface0
+  .high = {.bgra = {90, 71, 69, transparency}}, //Mocha Surface1
+  .swipe = {.bgra = {90, 71, 69, 64}}, //Mocha Surface1
+  .text = {.bgra = {244, 214, 205, transparency}}, //Mocha Text
+  .font = "Sans 20",
   .rounding = DEFAULT_ROUNDING,
 }
 };
